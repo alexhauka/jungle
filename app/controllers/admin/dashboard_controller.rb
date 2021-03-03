8af4cb1da282110af1f62d037987e5c29cbd80b0
alexhauka <alexhauka@gmail.com>
2021-03-03 21:53:20 +0000

    fix admin auth in env

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], except: [:index, :show]
+
   def show
   end
 end
