02cd02df3d4af2f810ea279ba5dab4858070704b
alexhauka <alexhauka@gmail.com>
2021-03-03 23:06:51 +0000

    add about page and nav link

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  
+  resources :about, only: [:index]
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -16,6 +18,9 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+
+  
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
