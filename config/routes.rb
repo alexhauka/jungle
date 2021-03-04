11e6a5dd274f560035c5fcef79d0191b107c7169
alexhauka <alexhauka@gmail.com>
2021-03-04 22:35:13 +0000

    implement register, login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,14 @@ Rails.application.routes.draw do
   
   resources :about, only: [:index]
 
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
+  resources :users
+  resources :sessions
+
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
