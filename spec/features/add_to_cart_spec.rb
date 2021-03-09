3002060d9eeb5e6525cc94103e7520022bda8d80
alexhauka <alexhauka@gmail.com>
2021-03-09 21:39:15 +0000

    add to cart spec

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to product details", type: :feature, js: true do
+RSpec.feature "User can add product to cart", type: :feature, JS: true do
   
   # SETUP
   before :each do
@@ -16,22 +16,20 @@ RSpec.feature "Visitor navigates to product details", type: :feature, js: true d
       )
     end
   end
-
-
-  scenario "They see product details" do
+  
+  
+  scenario "They see My Cart (1)" do
     # ACT
     visit root_path
 
     expect(page).to have_css 'article.product', count: 10
 
-    page.find('article.product:first-child').click_on "Details"
+    page.find('article.product:first-child').click_on "Add"
 
-    expect(page).to have_content("Name")
+    expect(page).to have_content("My Cart (1)")
 
-    save_screenshot
+    # save_screenshot
 
   end
-
-
 
 end
